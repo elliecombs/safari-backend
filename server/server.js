6a54b1f20a21ec011e5474b8c01b3f622a34ff52
Ellie Combs <96994266+elliecombs@users.noreply.github.com>
2023-02-13 17:49:32 +0100

    Update Server.js

--- server/server.js
+++ server/server.js
@@ -3,9 +3,20 @@ const app = express();
 const port = 8000;
 const cors = require('cors');
 
-app.use(cors({origin:"http://localhost:3000"}));
 require("./config/mongoose.config");
 app.use(express.json(), express.urlencoded({ extended: true }));
+// IF THE CLIENT URL IS FOUND IN ENV VARIABLES
+process.env.CLIENT_URL 
+// THEN CORS SETTINGS FOR PRODUCTION ENVIRONMENT
+? app.use(cors({
+    origin: process.env.CLIENT_URL,
+    preflightContinue: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials:true
+})) 
+// ELSE, IN DEV ENVIRONMENT, NO CORS SETTINGS NECESSARY
+: app.use(cors());
+
 const SafariRoutes = require("./routes/safari.route");
 SafariRoutes(app);
 
