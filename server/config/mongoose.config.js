9c641d044257dd263fa19757c0e7bed010640001
Ellie Combs <96994266+elliecombs@users.noreply.github.com>
2023-02-28 19:33:07 +0100

    server.js express

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/safari';
-mongoose.set('strictQuery', false);
+// mongoose.set('strictQuery', false);
 
 mongoose.connect(uri).then(() => console.log('Established a connection to the database')).catch(err => console.log('Something went wrong when connecting to the database ', err));
--- server/server.js
+++ server/server.js
@@ -8,17 +8,18 @@ app.use(express.json(), express.urlencoded({ extended: true }));
 // IF THE CLIENT URL IS FOUND IN ENV VARIABLES
 process.env.CLIENT_URL 
 // THEN CORS SETTINGS FOR PRODUCTION ENVIRONMENT
-? app.use(cors({
-    origin: process.env.CLIENT_URL,
-    preflightContinue: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials:true
-})) 
-// ELSE, IN DEV ENVIRONMENT, NO CORS SETTINGS NECESSARY
-: app.use(cors());
+    ? app.use(cors({
+        origin: process.env.CLIENT_URL,
+        preflightContinue: true,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials:true
+    })) 
+    // ELSE, IN DEV ENVIRONMENT, NO CORS SETTINGS NECESSARY
+    : app.use(cors());
 
 const SafariRoutes = require("./routes/safari.route");
 SafariRoutes(app);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`) );
+
 
